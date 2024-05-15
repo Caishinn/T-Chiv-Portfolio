9ad12dfe8adc75431335efc1b3fc6c1318056322
MORK Chiven <mork.chiven@optistech.com>
2024-05-15 14:46:23 +0700

    new

--- js/script.js
+++ js/script.js
@@ -15,8 +15,38 @@ document.addEventListener("DOMContentLoaded", function () {
   };
 
   const spawnParticles = () => {
-    setInterval(createParticle, 150);
+    setInterval(createParticle, 20);
   };
 
   spawnParticles();
 });
+
+/*--------text animation--------*/
+
+const txts = document.querySelector(".title").children,
+  txtsLen = txts.length;
+let index = 0;
+const textInTimer = 3000,
+  textOutTimer = 2800;
+
+function animateText() {
+  for (let i = 0; i < txtsLen; i++) {
+    txts[i].classList.remove("text-in", "text-out");
+  }
+  txts[index].classList.add("text-in");
+
+  setTimeout(function () {
+    txts[index].classList.add("text-out");
+  }, textOutTimer);
+
+  setTimeout(function () {
+    if (index == txtsLen - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+    animateText();
+  }, textInTimer);
+}
+
+window.onload = animateText;
